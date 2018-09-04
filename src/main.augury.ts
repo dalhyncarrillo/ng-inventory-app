300232321752646bf740cfe873a6131987679165
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2018-09-04 11:57:48 -0400

    augury

--- src/main.augury.ts
+++ src/main.augury.ts
@@ -1,21 +1,16 @@
-import { enableProdMode, NgZone } from '@angular/core';
+import { NgZone } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-// @todo: how to not include in prod bundle?
-// import { auguryBootstrap } from '../../../augury-labs/packages/core/dist'; // '@augury/core';
-// import { OverlayHealthIndicator } from '../../../augury-labs/packages/plugins/overlay-health-indicator/src';
+import { auguryBootstrap } from '/Users/andrew/Code/rangle/augury-labs/packages/core';
+import { PopoutZoneMonitor } from '/Users/andrew/Code/rangle/augury-labs/packages/plugins/popout-timeline';
 
-// auguryBootstrap({
-//   platform: platformBrowserDynamic,
-//   ngModule: AppModule,
-//   NgZone,
-//   plugins: [
-//     // new PerformanceProfilerPlugin(),
-//     // new PopoutZoneMonitor(),
-//     // new c.CDHeatMap(),
-//     new OverlayHealthIndicator(),
-//   ]
-// });
+auguryBootstrap({
+  platform: platformBrowserDynamic,
+  ngModule: AppModule,
+  NgZone,
+  plugins: [
+    new PopoutZoneMonitor(),
+  ]
+});
