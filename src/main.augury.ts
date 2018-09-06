2003e0ed4bc70f27f97f4388ad39a2323792832b
Andrew Thauer <6507159+andrewthauer@users.noreply.github.com>
2018-09-05 20:27:11 -0400

    update to use npm packages

--- src/main.augury.ts
+++ src/main.augury.ts
@@ -3,14 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-import { auguryBootstrap } from '/Users/andrew/Code/rangle/augury-labs/packages/core';
-import { PopoutZoneMonitor } from '/Users/andrew/Code/rangle/augury-labs/packages/plugins/popout-timeline';
+import { auguryBootstrap } from '@augury/core';
+import { PerformanceProfilerPlugin } from '@augury/performance-profiler-plugin';
 
 auguryBootstrap({
   platform: platformBrowserDynamic,
   ngModule: AppModule,
   NgZone,
   plugins: [
-    new PopoutZoneMonitor(),
+    new PerformanceProfilerPlugin(),
   ]
 });
